0be421fdaf1d09740dce30e9e8a6fd8dd8824e5a
Moneeb919 <99467393+Moneeb919@users.noreply.github.com>
2024-01-12 01:14:36 +0530

    delete channels

--- components/providers/modal-providers.tsx
+++ components/providers/modal-providers.tsx
@@ -8,6 +8,7 @@ import { MembersModal } from "../modals/members-modal"
 import { CreateChannelModal } from "../modals/create-channel-modal"
 import { LeaveServerModal } from "../modals/leave-server-modal"
 import { DeleteServerModal } from "../modals/delete-server-modal"
+import { DeleteChannelModal } from "../modals/delete-channel-modal"
 
 export const ModalProviders = () => {
     const [isMounted, setIsMounted] = useState<boolean>(false);
@@ -29,6 +30,7 @@ export const ModalProviders = () => {
             <CreateChannelModal />
             <LeaveServerModal />
             <DeleteServerModal />
+            <DeleteChannelModal />
         </>
     )
 }
