fb6713afc53a192b89ba469abc7a23621506adbd
Moneeb919 <99467393+Moneeb919@users.noreply.github.com>
2024-01-30 04:00:24 +0530

    message attachment

--- components/file-upload.tsx
+++ components/file-upload.tsx
@@ -1,39 +1,68 @@
-"use client"
+"use client";
 
-import { UploadDropzone } from "@/lib/uploadthing"
-import "@uploadthing/react/styles.css"
-import { X } from "lucide-react"
-import Image from "next/image"
+import { UploadDropzone } from "@/lib/uploadthing";
+import "@uploadthing/react/styles.css";
+import { X, FileIcon } from "lucide-react";
+import Image from "next/image";
 
 interface FileUploadProps {
-    onChange: (url?: string) => void
-    value: string
-    endpoint: "serverImage" | "messageImage"
+  onChange: (url?: string) => void;
+  value: string;
+  endpoint: "serverImage" | "messageFile";
 }
 
 export const FileUpload = ({ endpoint, value, onChange }: FileUploadProps) => {
-    const fileType = value?.split(".").pop();
+  const fileType = value?.split(".").pop();
 
-    if (value && fileType !== "pdf") {
-        return (
-            <div className="relative h-20 w-20">
-                <Image className="rounded-full" fill src={value} alt="Upload" />
-                <button onClick={() => onChange("")} className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm" type="button">
-                    <X className="h-4 w-4" />
-                </button>
-            </div>
-        )
-    }
-
+  if (value && fileType !== "pdf") {
     return (
-        <div>
-            <UploadDropzone endpoint="serverImage" onClientUploadComplete={(res) => {
-                onChange(res?.[0].url);
-            }}
-                onUploadError={(error: Error) => {
-                    console.log(error);
-                }}
-            />
-        </div>
-    )
-}
+      <div className="relative h-20 w-20">
+        <Image className="rounded-full" fill src={value} alt="Upload" />
+        <button
+          onClick={() => onChange("")}
+          className="bg-rose-500 text-white p-1 rounded-full absolute top-0 right-0 shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
+
+  if (value && fileType === "pdf") {
+    return (
+      <div className="relative flex items-center p-2 mt-2 rounded-md bg-background/10">
+        <FileIcon className="h-10 w-10 fill-indigo-200 stroke-indigo-400" />
+        <a
+          href={value}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="ml-2 text-sm text-indigo-500 dark:text-indigo-400 hover:underline"
+        >
+          {value}
+        </a>
+        <button
+          onClick={() => onChange("")}
+          className="bg-rose-500 text-white p-1 rounded-full absolute -top-2 -right-2 shadow-sm"
+          type="button"
+        >
+          <X className="h-4 w-4" />
+        </button>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <UploadDropzone
+        endpoint="serverImage"
+        onClientUploadComplete={(res) => {
+          onChange(res?.[0].url);
+        }}
+        onUploadError={(error: Error) => {
+          console.log("THERE IS AN ERRRROORRR", error);
+        }}
+      />
+    </div>
+  );
+};
