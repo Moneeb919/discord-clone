5a17f29058aed56d03d40ad64daedf20f9549df0
Moneeb919 <99467393+Moneeb919@users.noreply.github.com>
2024-01-31 03:44:30 +0530

    chat messages component

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { ThemeProvider } from "@/components/providers/theme-provider";
 import { cn } from "@/lib/utils";
 import { SocketProvider } from "@/components/providers/socket-providers";
 import { ModalProviders } from "@/components/providers/modal-providers";
+import { QueryProvider } from "@/components/providers/query-providers";
 
 const inter = Open_Sans({ subsets: ["latin"] });
 
@@ -32,7 +33,7 @@ export default function RootLayout({
           >
             <SocketProvider>
               <ModalProviders />
-              {children}
+              <QueryProvider>{children}</QueryProvider>
             </SocketProvider>
           </ThemeProvider>
         </body>
